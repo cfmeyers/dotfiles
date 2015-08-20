c041307ede95805e0b6418654a8e72aca1f97483
Collin Meyers <cfmeyers@gmail.com>
2015-08-20 16:20:02 -0400

    update tools alias work computer

--- zsh/work_computer.sh
+++ zsh/work_computer.sh
@@ -4,6 +4,6 @@
 # ====================================
 eval "$(pyenv init -)"
 
-alias tools="ssh -A -i ~/.ssh/test_id_rsa collin@tools.wineapi.com"
+alias tools="ssh -A -i ~/.ssh/id_rsa_test collin@tools.wineapi.com"
 
 pyenv virtualenvwrapper
