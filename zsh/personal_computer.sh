9faa587fd4da1ead6755cc7b28cfd36db266fbd0
Collin Meyers <cfmeyers@gmail.com>
2015-10-23 21:55:48 -0400

    Remove unneeded aliases from personal_computer.sh

--- zsh/personal_computer.sh
+++ zsh/personal_computer.sh
@@ -1,18 +1,11 @@
-# Personal Computer
-source /usr/local/bin/virtualenvwrapper.sh
-# ====================================
-# PyEnv
-# ====================================
-eval "$(pyenv init -)"
 
-export ZELDA="/Users/collin/dev/Princess_Zelda/"
-alias anki='vim /Users/collin/Dropbox/misc/anki.md'
 alias nb='newsbeuter'
-alias tw="$HOME/.rbenv/versions/2.2.0/bin/t"
 
-alias t='/usr/local/Cellar/todo-txt/2.10/bin/todo.sh -td $HOME/Dropbox/todo/todo.cfg' #Todo.txt alias
-alias tls='clear; /usr/local/Cellar/todo-txt/2.10/bin/todo.sh -td $HOME/Dropbox/todo/todo.cfg ls' #
 alias tools="ssh -A -i ~/.ssh/testlaptop_id_rsa collin@tools.wineapi.com"
 
 alias killmanage="ps aux | grep manage.py | awk '{print \$2}' | xargs kill"
-source ~/.secret_config
+#source ~/.secret_config
+
+
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
