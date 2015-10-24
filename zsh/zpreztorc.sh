72ae1e44fa8a57081735590d0d871a1293eed773
Collin Meyers <cfmeyers@gmail.com>
2015-10-24 14:51:32 -0400

    Add dotfile for prezto zsh framework

---
+++ zsh/zpreztorc.sh
@@ -0,0 +1,21 @@
+zstyle ':prezto:*:*' color 'yes'
+zstyle ':prezto:module:syntax-highlighting' color 'yes'
+
+zstyle ':prezto:load' pmodule \
+  'environment' \
+  'terminal' \
+  'editor' \
+  'history' \
+  'directory' \
+  'spectrum' \
+  'utility' \
+  'completion' \
+  'git' \
+  'history-substring-search'\
+  'syntax-highlighting' \
+  'prompt'
+
+zstyle ':prezto:module:editor' key-bindings 'emacs'
+
+zstyle ':prezto:module:prompt' theme 'sorin'
+
