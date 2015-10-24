5252c38ef2e9af4de269e5bfa545b1735e2425f6
Collin Meyers <cfmeyers@gmail.com>
2015-10-24 18:14:20 -0400

    Reorganzie zshrc

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,112 +1,102 @@
-
-#installing Prezto
-setopt EXTENDED_GLOB
-#for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-#  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-#done
-
-
-# Source Prezto.
+# ============== #
+# Prezto Configs #
+# ============== #
+setopt EXTENDED_GLOB #for Prezto
 if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
-
-
-#unsetopt correct_all 
 unsetopt CORRECT #turn off zsh correct prompt
 
-alias secret="vim ~/.secret_config"
-HISTIGNORE="jrnl *"
 
-# ZSH config Aliases
-alias zshconfig="vim ~/dotfiles/zsh/zshrc.sh"
-alias zconfig="vim ~/dotfiles/zsh/zshrc.sh"
-alias zshreload="source ~/.zshrc; echo sourcing zshrc!"
+# ====================== #
+# Config Editing Aliases #
+# ====================== #
 alias zreload="source ~/.zshrc; echo sourcing zshrc!"
 alias reload="source ~/.zshrc; echo sourcing zshrc!"
+alias zconfig="vim ~/dotfiles/zsh/zshrc.sh"
 alias vconfig="vim ~/dotfiles/vim/vimrc.vim"
 alias tconfig="vim ~/dotfiles/tmux/tmux.conf"
 alias nconfig="vim ~/dotfiles/nosetests/noserc"
 
+# =============================== #
+# Better Shell Experience Aliases #
+# =============================== #
+alias celar="clear"
+alias c='pygmentize -O style=monokai -f console256 -g'
+alias disable_ethernet="sudo ifconfig en0 down"
+alias enable_ethernet="sudo ifconfig en0 up"
+alias ls='ls -F' # show slashes for directories.
+alias history='history 0' 
+alias chrome='open -a "Google Chrome"'
 alias cd..="cd .."
 alias cd...="cd ../.."
 alias cd....="cd ../../.."
 alias vi="vim"
 
-export VISUAL=vim
-export EDITOR="$VISUAL"
 
-
-#Misc Aliases
-alias celar="clear"
-alias c="clear"
-alias video="sudo killall VDCAssistant"
-alias c='pygmentize -O style=monokai -f console256 -g'
-alias disable_ethernet="sudo ifconfig en0 down"
-alias enable_ethernet="sudo ifconfig en0 up"
+# ==================== #
+# Quick Access Aliases #
+# ==================== #
 alias work="cd ~/dev/clubs"
 alias dots="cd ~/dotfiles"
-alias scratch="vim ~/practice/python/scratch.py"
+alias secret="vim ~/.secret_config"
+alias scratch="vim ~/scratch.py"
 
-##PATH
-PATH="/usr/local/share/npm/bin:$PATH"                      # NPM
-PATH="/usr/local/bin:/usr/local/sbin:$PATH"                # Homebrew
-PATH="/usr/local/heroku/bin:$PATH"                        # Heroku Toolbelt
-PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"      # Coreutils
-PATH="$HOME/.rbenv/bin:$PATH"                              # RBENV
+# ========= #
+# PATH mods #
+# ========= #
+PATH="/usr/local/share/npm/bin:$PATH"                # NPM
+PATH="/usr/local/bin:/usr/local/sbin:$PATH"          # Homebrew
+PATH="/usr/local/heroku/bin:$PATH"                   # Heroku Toolbelt
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH" # Coreutils
+PATH="$HOME/.rbenv/bin:$PATH"                        # RBENV
 
 MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH" # Manual pages
 
+# ======== #
+# Settings #
+# ======== #
+export LC_ALL="en_US.UTF-8" # Prefer US English
+export LANG="en_US" # use UTF-8
+export VISUAL=vim
+export EDITOR="$VISUAL"
 
-eval "$(rbenv init -)" #for RBENV
-
-# =================
-# Settings
-# =================
-
-# Prefer US English
-export LC_ALL="en_US.UTF-8"
-# use UTF-8
-export LANG="en_US"
-
-# =================
-# History
-# =================
-
-# http://jorge.fbarr.net/2011/03/24/making-your-bash-history-more-efficient/
-# Larger bash history (allow 32³ entries; default is 500)
+# ======= #
+# History #
+# ======= #
 export HISTSIZE=32768
 export HISTFILESIZE=$HISTSIZE
+# http://jorge.fbarr.net/2011/03/24/making-your-bash-history-more-efficient/
+# Larger bash history (allow 32³ entries; default is 500)
+setopt inc_append_history # Appends every command to the history file once it is executed
+setopt share_history # Reloads the history whenever you use it
+HISTIGNORE="jrnl *"
 
-# Appends every command to the history file once it is executed
-setopt inc_append_history
-# Reloads the history whenever you use it
-setopt share_history
 
-
-# ====================
-# Aliases
-# ====================
+# ============ #
+# Work Aliases #
+# ============ #
 alias run='python manage.py runserver --env mydev'
 alias runforbes='python manage.py runserver --site forbes --env mydev'
 alias rebuild='python manage.py rebuilddb --env mydev'
+alias nt='nosetests --with-id'
+alias fo='nosetests --failed'
+
+
+# ========== #
+# Ag Aliases #
+# ========== #
 alias pag='ag --python --pager=less'
 alias jag='ag --jade --pager=less'
 alias cag='ag --coffee --pager=less'
 alias tag='ag -G test --pager=less'
 
-# show slashes for directories.
-alias ls='ls -F'
 
-alias history='history 0' 
 
-alias chrome='open -a "Google Chrome"'
-
-# =================
-# rbenv
-# =================
-
-# start rbenv (our Ruby environment and version manager) on open
+# ===== #
+# rbenv #
+# ===== #
+eval "$(rbenv init -)" #for RBENV
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
 
@@ -125,20 +115,12 @@ else
     source ~/dotfiles/zsh/work_computer.sh
 fi
 
-# =================
-# Functions
-# =================
+# ========= #
+# Functions #
+# ========= #
 
 function google() { open /Applications/Google\ Chrome.app/ "http://www.google.com/search?q= $1"; }
 
 function firefox() { open -a /Applications/Firefox.app/ "http://www.google.com/search?q= $1"; }
 
-function collin() {
-    ls /usr/local/bin/collin/
-
-}
-
-function mkcd () {
-    mkdir -p "$*"
-    cd "$*"
-}
+function mkcd () { mkdir -p "$*"; cd "$*" }
